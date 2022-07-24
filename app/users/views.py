ebadb933e7113efe7e5c9a63de88387268e9975c
Sbtah <shisastore@protonmail.com>
2022-07-24 21:13:58 +0200

    Added ManageUserView that handles authenticated user's requests to profiles

--- app/users/views.py
+++ app/users/views.py
@@ -1,7 +1,7 @@
 """
 Views for the User API.
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from users.serializers import (
@@ -21,3 +21,15 @@ class CreateTokenView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class MenageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenicated user."""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrive and return the authenticated user."""
+        return self.request.user
