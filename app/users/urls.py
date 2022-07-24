4e7e09c6050752cb2f458367438930f34c233603
Sbtah <shisastore@protonmail.com>
2022-07-24 21:15:36 +0200

    Added url for me/ endpoint

--- app/users/urls.py
+++ app/users/urls.py
@@ -11,4 +11,5 @@ app_name = 'users'
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.MenageUserView.as_view(), name='me'),
 ]
