c087a599cbb558852b4ac50255bb88bc817f5d9c
Sbtah <shisastore@protonmail.com>
2023-02-12 21:56:16 +0100

    Added view.py for recipes app with RecipeViewSet.

---
+++ app/recipes/views.py
@@ -0,0 +1,23 @@
+"""
+Views for recipe APIs.
+"""
+from recipes import serializers
+from recipes.models import Recipe
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """
+    View for manage recipe APIs.
+    """
+
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        "Retrive recipes for authenticated user."
+        return self.queryset.filter(user=self.request.user).order_by("-id")
