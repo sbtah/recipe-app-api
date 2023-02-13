33e9a261a4cfca99c6232cd0cae32e04a8948592
Sbtah <shisastore@protonmail.com>
2023-02-13 22:14:33 +0100

    Added get_serializer_class in RecipeViewSet to feed different serializer for detail API.

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -13,7 +13,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     View for manage recipe APIs.
     """
 
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -21,3 +21,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         "Retrive recipes for authenticated user."
         return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """Return serializer for request."""
+        if self.action == "list":
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
