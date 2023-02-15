e418cb9bec2a142caf18ca2d59e9fff0e16188a5
Sbtah <shisastore@protonmail.com>
2023-02-15 21:11:00 +0100

    Added Tag model.

--- app/tags/models.py
+++ app/tags/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Tag(models.Model):
+    """Tag object for filtering recipes."""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
