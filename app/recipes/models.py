fd6fbcd3ee13867a71a135a54c99468eb20ded8c
Sbtah <shisastore@protonmail.com>
2023-02-15 21:12:21 +0100

    Added M2M relation with Tags on Recipe model.

--- app/recipes/models.py
+++ app/recipes/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from tags.models import Tag
 
 
 class Recipe(models.Model):
@@ -14,6 +15,7 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
