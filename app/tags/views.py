e9ed3aa06154c3fd84cc138b1d13f053587e7ad6
Sbtah <shisastore@protonmail.com>
2023-02-15 22:25:17 +0100

    Added TagViewSet for Tags.

--- app/tags/views.py
+++ app/tags/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render  # noqa
+"""Views for Tags objects."""
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from tags.models import Tag
+from tags.serializers import TagSerializer
 
-# Create your views here.
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage Tags in the database."""
+
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        "Retrive Tags for authenticated user."
+        return self.queryset.filter(user=self.request.user).order_by("-name")
