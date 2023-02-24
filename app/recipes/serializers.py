1cb2080a0e1783881d56943e89252fba9cc38c40
Sbtah <shisastore@protonmail.com>
2023-02-24 12:10:04 +0100

    Update RecipeSerializer with nested TagSerializer.

--- app/recipes/serializers.py
+++ app/recipes/serializers.py
@@ -1,15 +1,31 @@
 """Serilizers for recipe APIs."""
 from rest_framework import serializers
 from recipes.models import Recipe
+from tags.models import Tag
+from tags.serializers import TagSerializer
 
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
+    tags = TagSerializer(many=True, required=False)
+
     class Meta:
         model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "link"]
+        fields = ["id", "title", "time_minutes", "price", "link", "tags"]
         read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        """Create a Recipe and or create Tags if any."""
+
+        tags = validated_data.pop("tags", [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context["request"].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
