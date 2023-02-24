139da9bd84f963ea952778a414da4587b4591e5b
Sbtah <shisastore@protonmail.com>
2023-02-24 15:08:46 +0100

    Added update and _get_or_create_tags methods to RecipeSerializer.

--- app/recipes/serializers.py
+++ app/recipes/serializers.py
@@ -15,17 +15,34 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ["id", "title", "time_minutes", "price", "link", "tags"]
         read_only_fields = ["id"]
 
+    def _get_or_create_tags(self, tags, recipe):
+        auth_user = self.context["request"].user
+        for tag in tags:
+            tag_obj, _ = Tag.objects.get_or_create(user=auth_user, **tag)
+            recipe.tags.add(tag_obj)
+
     def create(self, validated_data):
         """Create a Recipe and or create Tags if any."""
 
         tags = validated_data.pop("tags", [])
         recipe = Recipe.objects.create(**validated_data)
-        auth_user = self.context["request"].user
-        for tag in tags:
-            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
-            recipe.tags.add(tag_obj)
+        self._get_or_create_tags(tags, recipe)
 
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update method."""
+
+        tags = validated_data.pop("tags", None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
