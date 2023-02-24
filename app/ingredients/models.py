3af26018a32242de61541b5ccfbfb0a60d61f336
Sbtah <shisastore@protonmail.com>
2023-02-24 15:51:22 +0100

    Added Ingredient class in models.py.

--- app/ingredients/models.py
+++ app/ingredients/models.py
@@ -1,3 +1,18 @@
 """
-Ingredients models.
+Ingredient model.
 """
+from django.db import models
+from django.conf import settings
+
+
+class Ingredient(models.Model):
+    """Ingredient object."""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
