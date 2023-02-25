0bc074b65c32afe40272666ed95a9fccd84064e1
Sbtah <shisastore@protonmail.com>
2023-02-25 16:47:26 +0100

    Added urls for Ingredients enpoints.

--- app/recipes/urls.py
+++ app/recipes/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 from recipes.views import RecipeViewSet
 from rest_framework.routers import DefaultRouter
 from tags.views import TagViewSet
+from ingredients.views import IngredientViewSet
 
 
 app_name = "recipes"
@@ -13,7 +14,7 @@ app_name = "recipes"
 router = DefaultRouter()
 router.register("recipes", RecipeViewSet)
 router.register("tags", TagViewSet)
-
+router.register("ingredients", IngredientViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
