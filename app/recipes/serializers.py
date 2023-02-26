6df630e0d4f404af1ef9d0ac9b40f562afb7a929
Sbtah <shisastore@protonmail.com>
2023-02-26 20:25:57 +0100

    Update RecipeSerializer's update method.

--- app/recipes/serializers.py
+++ app/recipes/serializers.py
@@ -59,9 +59,15 @@ class RecipeSerializer(serializers.ModelSerializer):
         """Update method."""
 
         tags = validated_data.pop("tags", None)
+        ingredients = validated_data.pop("ingredients", None)
+
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
