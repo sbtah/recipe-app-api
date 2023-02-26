2801d1de11fd175907f9f0b20f4e3b581dbbe7c2
Sbtah <shisastore@protonmail.com>
2023-02-26 17:20:15 +0100

    Update RecipeSerializer with nested IngredientSerializer. Added _get_or_create_ingredients internal helper method on RecipeSerializer that is being used in create method.

--- app/recipes/serializers.py
+++ app/recipes/serializers.py
@@ -1,6 +1,8 @@
 """Serilizers for recipe APIs."""
-from rest_framework import serializers
+from ingredients.models import Ingredient
+from ingredients.serializers import IngredientSerializer
 from recipes.models import Recipe
+from rest_framework import serializers
 from tags.models import Tag
 from tags.serializers import TagSerializer
 
@@ -9,24 +11,47 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "link", "tags"]
+        fields = [
+            "id",
+            "title",
+            "time_minutes",
+            "price",
+            "link",
+            "tags",
+            "ingredients",
+        ]
         read_only_fields = ["id"]
 
     def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating Tags."""
+
         auth_user = self.context["request"].user
         for tag in tags:
             tag_obj, _ = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or creating Ingredients."""
+
+        auth_user = self.context["request"].user
+        for ingredient in ingredients:
+            ingredient_obj, _ = Ingredient.objects.get_or_create(
+                user=auth_user, **ingredient
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Create a Recipe and or create Tags if any."""
 
         tags = validated_data.pop("tags", [])
+        ingredients = validated_data.pop("ingredients", [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
