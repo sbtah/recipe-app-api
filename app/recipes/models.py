26ca0dc56efffb434e747fa9767ccb9ae580918d
Sbtah <shisastore@protonmail.com>
2023-02-27 22:01:30 +0100

    Added recipe_image_file_path helper function. Added ImageField to Recipe model.

--- app/recipes/models.py
+++ app/recipes/models.py
@@ -2,6 +2,17 @@ from django.conf import settings
 from django.db import models
 from tags.models import Tag
 from ingredients.models import Ingredient
+import uuid
+import os
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate filepath for new Recipe image."""
+
+    ext = os.path.splitext(filename)[1]
+    filename = f"{uuid.uuid4()}{ext}"
+
+    return os.path.join("uploads", "recipe", filename)
 
 
 class Recipe(models.Model):
@@ -18,6 +29,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField(Tag)
     ingredients = models.ManyToManyField(Ingredient)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
