030f680ffa858cf6954e35f1046125b8fac16d41
Sbtah <shisastore@protonmail.com>
2023-02-28 21:15:52 +0100

    Added settings for drf spectacular.

--- app/core/settings.py
+++ app/core/settings.py
@@ -134,3 +134,8 @@ AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
+
+SPECTACULAR_SETTINGS = {
+    "COMPONENT_SPLIT_REQUEST": True,
+}
