09379a79e142b980480eea5fa9426fbcafb60e12
Sbtah <shisastore@protonmail.com>
2023-02-28 21:16:56 +0100

    Added custom action: upload_image. Edited get_serializer_class.

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -3,9 +3,11 @@ Views for recipe APIs.
 """
 from recipes import serializers
 from recipes.models import Recipe
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -26,12 +28,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return serializer for request."""
         if self.action == "list":
             return serializers.RecipeSerializer
+        elif self.action == "upload_image":
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        """Upload an image to Recipe."""
+
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(
