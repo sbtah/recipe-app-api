f0c20ff223ed4843c00edbc753cfb64ce96dd12e
Sbtah <shisastore@protonmail.com>
2023-03-07 16:07:50 +0100

    Added health_check view.

---
+++ app/core/views.py
@@ -0,0 +1,12 @@
+"""
+Core views for app
+"""
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def health_check(request):
+    """Return successful response."""
+
+    return Response({"healthy": True})
